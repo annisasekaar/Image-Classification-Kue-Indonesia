5931c9847f33f0b92eecdc8ac5272c6c5d9ba76d
Sekar <annisayuningtyas28@yahoo.com>
2020-07-03 16:30:47 +0700

    modify responsive page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import * as tf from '@tensorflow/tfjs';
 import 'bootstrap/dist/css/bootstrap.css';
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
-
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
       constructor(props){
         super(props);
         this.state = {
@@ -15,16 +12,16 @@ class App extends React.Component {
           imagePreviewUrl: null,
           pred: "",
           model : null,
+          speed: 1,
         }
         //this.prediction = null;
         this.handleChange = this.handleChange.bind(this);
         this.modelPredict = this.modelPredict.bind(this);
         this.modelReady = this.modelReady.bind(this);
-        //this.loadsBISIMILLAH = this.loadsBISIMILLAH.bind(this);
       };
 
       componentDidMount() {
-        let modelSementara = this.loadss();
+        let modelSementara = this.modelLoad();
         modelSementara.then((res)=>{
           this.setState({
             model : res,
@@ -32,7 +29,7 @@ class App extends React.Component {
         });
       };
 
-      loadss() {
+      modelLoad() {
         console.log('Loading data');
         return tf.loadLayersModel('./tfjs_model/model.json');
       };
@@ -40,53 +37,56 @@ class App extends React.Component {
       modelReady = () => {
         if (this.state.model) {
           return (
-            <div>
-              <h3>MODEL READY</h3>
+            <div className="model-section">
+              <p>Model Ready</p>
+              <div>
+                <input className="button" type="file" onChange={this.handleChange} />
+              </div>
+              <div>{this.fileData()}</div>
+              <button className="button" onClick={this.modelPredict}> check my answer </button>
+              <div>{this.dataPredict()}</div>
             </div>
           )} else {
             return (
-            <div>
-              <h2>LOADING MODEL............</h2>
+            <div className="model-section">
+              <p>Loading Model...</p>
+              <img style={{animation: `spin ${this.state.speed}s linear infinite`}} src={'./restart_white.png'} alt="wait" height="20"/>
             </div>
-          )}
+          )};
       };
-
 
       // Load model tf.js
       modelPredict = async () => {
+        if (this.state.selectedFile) {
+          const label = ['kue dadar gulung', 'kue kastengel', 'kue klepon', 'kue lapis', 'kue lumpur', 'kue putri salju', 'kue risoles', 'kue serabi'];
 
-        const label = ['kue dadar gulung', 'kue kastengel', 'kue klepon', 'kue lapis', 'kue lumpur', 'kue putri salju', 'kue risoles', 'kue serabi'];
-        // let model_path = './tfjs_model/model.json';
-        // console.log('loading model .................');
-        //
-        // const model_tfjs = await tf.loadLayersModel(model_path);
-        // //load model
-        // console.log(model_tfjs.summary());
+          const IMAGE_SIZE = 150;
 
-        const IMAGE_SIZE = 150;
+          const logits = tf.tidy(() => {
+            const normalizationConstant = 1.0 / 255.0;
+            let img = tf.browser.fromPixels(this.photoRef)
+                        .resizeBilinear([IMAGE_SIZE, IMAGE_SIZE], false)
+                        .expandDims(0)
+                        .toFloat()
+                        .mul(normalizationConstant)
 
-        const logits = tf.tidy(() => {
-          const normalizationConstant = 1.0 / 255.0;
-          let img = tf.browser.fromPixels(this.photoRef)
-                      .resizeBilinear([IMAGE_SIZE, IMAGE_SIZE], false)
-                      .expandDims(0)
-                      .toFloat()
-                      .mul(normalizationConstant)
+            console.log(this.state.model.predict(img).print());
 
-          console.log(this.state.model.predict(img).print());
+            return this.state.model.predict(img);
+          });
 
-          return this.state.model.predict(img);
-        });
-
-        const classes = await logits.data();
-        const prediction = await label[classes.indexOf(Math.max(...classes))];
-        this.setState({
-          pred: prediction
-        });
-        //console.log(classes.indexOf(Math.max(...classes)));
-        //console.log(label[classes.indexOf(Math.max(...classes))]);
-        console.log(this.state.pred);
-
+          const classes = await logits.data();
+          const prediction = await label[classes.indexOf(Math.max(...classes))];
+          this.setState({
+            pred: prediction,
+          });
+          //console.log(classes.indexOf(Math.max(...classes)));
+          //console.log(label[classes.indexOf(Math.max(...classes))]);
+          console.log(this.state.pred);
+        } else {
+          // to handle error in predict null image
+          alert("The file cannot be empty!");
+        }
       };
 
       // On file select (from the pop up)
@@ -107,25 +107,23 @@ class App extends React.Component {
       dataPredict = () => {
         if (this.state.pred) {
           return (
-            <div>
+            <div className="predict">
               <h2>{this.state.pred}</h2>
             </div>
-          );}
+          )};
       };
 
-      // File content to be displayed after
-      // file upload is complete
       fileData = () => {
         if (this.state.selectedFile) {
           return (
-            <div>
-              <h4>Image:</h4>
-              <img src={this.state.imagePreviewUrl} alt="preview_gambar" ref={(ref)=>{this.photoRef = ref}} width="300" height="300" />
+            <div className="show-file">
+              <p>Image:</p>
+              <img className="preview_gambar" src={this.state.imagePreviewUrl} alt="preview_gambar" ref={(ref)=>{this.photoRef = ref}} />
             </div>
           );} else {
           return (
-            <div>
-              <h5>Choose file before Pressing the Upload button</h5>
+            <div className="show-file">
+              <p>Choose file before click the button below</p>
             </div>
           );}
       };
@@ -133,29 +131,25 @@ class App extends React.Component {
   render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1> Guessing Indonesian Traditional Snack </h1>
-          <div>
-            Insert a photo of one of these snacks, and I'll guess the name of the snack!
-            <h6>kue dadar gulung - kue kastengel - kue klepon - kue lapis - kue lumpur - kue putri salju - kue risoles - kue serabi</h6>
+        <div className="App-header">
+          <div className="Title">
+            <h1> Guessing Indonesian Traditional Snack </h1>
+            <div>
+              Insert a photo of one of these snacks, and I'll guess the name of the snack!
+              <p className="nama-kue">kue dadar gulung - kue kastengel - kue klepon - kue lapis - kue lumpur - kue putri salju - kue risoles - kue serabi</p>
+            </div>
+            <div>
+              <img className="Image" src={'./dessert_icon.png'} height="auto" alt="logo"/>
+            </div>
           </div>
-          <img src={'./dessert_icon.png'} alt="logo" height="200" />
-          <div>
-            <input type="file" onChange={this.handleChange}  />
-          </div>
-          {this.fileData()}
-          <div>
-            {this.modelReady()}
-            <button onClick={this.modelPredict}> CHECK MY ANSWER </button> <br/>
-            {this.dataPredict()}
-          </div>
-        </header>
-          <div><br/>
-            Final Project Bangk!t Academy <br/>
-            Using EfficientNet Model for CNN Image Classification <br/>
-            By : Annisa Sekar A & Ilham Firdausi Putra <br/><br/>
-          </div>
-          Icons made by <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">iconixar</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+          <div className="Machine-Learning-Section"> {this.modelReady()} </div>
+        </div>
+        <div className="Footer">
+          Final Project Bangk!t Academy <br/>
+          Using EfficientNet Model for CNN Image Classification <br/>
+          By : Annisa Sekar Ayuningtyas & Ilham Firdausi Putra <br/><br/>
+          Icons made by <a href="https://www.flaticon.com/" title="flaticon"> www.flaticon.com</a>
+        </div>
       </div>
 
 
